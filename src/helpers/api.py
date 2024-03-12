f1479e83bbb8092f0335f4ca693944e5eabc85a9
touhiDroid <tz08128@gmail.com>
2024-03-12 18:33:55 -0400

    server-stat & device-info apis done

--- src/helpers/api.py
+++ src/helpers/api.py
@@ -1,9 +1,12 @@
+import json
 import os
 from sys import stderr
 
 import requests
 from dotenv import load_dotenv, set_key
 from urllib3.exceptions import NewConnectionError, MaxRetryError
+
+from src.models.EspDevice import EspDevice
 
 load_dotenv()
 
@@ -50,47 +53,26 @@ def post_next_action_label(class_name):
 
 def get_server_stats():
     try:
-        resp = requests.get(f'{get_server_address()}/device-metrics', params=None)
-        # Json Format: {
-        #             'data_directory': self.config.data_dir,
-        #             'navigation': self.config.navigation,
-        #             'notes': load_from_file(self.config.data_file_names['notes']),
-        #             'is_csi_enabled': get_is_csi_enabled(self.config),
-        #             'tty_plugins': self.config.tty_plugins,
-        #             'storage': {
-        #                 'used': shutil.disk_usage("/").used,
-        #                 'total': shutil.disk_usage("/").total,
-        #             },
-        #             'devices': [d.device_path for d in self.config.devices],
-        #             'hostname': socket.gethostname(),
-        #         }
+        resp = requests.get(f'{get_server_address()}/server-stats', params=None)
         if resp is not None and resp.status_code == 200:
-            print(resp)  # TODO Parse data-dir & list of devices
+            j = json.loads(resp.content)
+            return (j['data_directory'],
+                    j['storage']['used'],
+                    j['storage']['total'],
+                    j['devices'])
     except (ConnectionError, ConnectionRefusedError, NewConnectionError, MaxRetryError, Exception) as err:
         resp = None
         stderr.write(str(err) + "\n")
         pass
-    return resp
+    return None
 
 
-def get_esp_device_details(device_name):
+def get_esp_device_details(device_name) -> EspDevice | None:
     params = (('device_name', device_name),)
     try:
         resp = requests.get(f'{get_server_address()}/device-metrics', params=params)
-        # resp JSON format = {
-        # 'status': 'OK',
-        # 'device_name': device_name,
-        # 'file': self.config.data_file_names[device_name],
-        # 'application': load_from_file(f'/tmp/application/{device_name}'),
-        # 'wifi_channel': load_from_file(f'/tmp/wifi_channel/{device_name}'),
-        # 'data_rate': os.popen(f'tail -n 60 /tmp/data_rates/{device_name}').read(),
-        # 'files_size': os.path.getsize(self.config.data_file_names[device_name]),
-        # 'most_recent_csi': {
-        #     'samples': os.popen(f'tail -n 3 {self.config.data_file_names[device_name]}').read(),
-        # }
         if resp is not None and resp.status_code == 200:
-            print(resp)  # TODO Parse & return device metrics (path, channel, data-rate & file-size)
-
+            return EspDevice.from_json(resp.content)
     except (ConnectionError, ConnectionRefusedError, NewConnectionError, MaxRetryError, Exception) as err:
         resp = None
         stderr.write(str(err) + "\n")
--- src/models/EspDevice.py
+++ src/models/EspDevice.py
@@ -1,7 +1,31 @@
+import json
+
+from src.helpers import format_bytes
+
+
 class EspDevice(object):
-    def __init__(self, path, channel, tx_rate, file_size):
+    def __init__(self, name, channel, tx_rate, file_size):
         super().__init__()
-        self.path = path
+        self.name = name
         self.channel = channel
         self.tx_rate = tx_rate
         self.file_size = file_size
+
+    @staticmethod
+    def from_json(json_str):
+        j = json.loads(json_str)
+        name = j['device_name']
+        ch = j['wifi_channel']
+        tss = j['data_rate'].split('\n')
+        tx_rate = int(tss[-2].split(',')[1])
+        file_size = j['files_size']
+        return EspDevice(name, ch, tx_rate, file_size)
+
+    @staticmethod
+    def get_list_to_str(devices):
+        i = 1
+        _str = ""
+        for d in devices:
+            _str += f"{i}. {d.name[d.name.rindex('/') + 1:]}, Ch-{d.channel}, {format_bytes(d.file_size)}, {d.tx_rate}Hz\n"
+            i += 1
+        return _str
